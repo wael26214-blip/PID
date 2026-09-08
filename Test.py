eadadba323a83367668dd63f71d013e697284d0b
wael26214-blip <wael26214@gmail.com>
2026-09-08 20:09:01 +0300

    Refactor PID simulation loop to use while instead of for

--- Test.py
+++ Test.py
@@ -3,6 +3,7 @@ from PIDController import *
 
 if __name__ == "__main__":
     pid = PIDController(
+                        dt=0.1,
                         kp=3,
                         ki=0.1,
                         kd=0.2,
@@ -11,22 +12,29 @@ if __name__ == "__main__":
                         )
     target_setpoint = 15.0
     current_state = 0.0
+    dt = pid.dt
     
     pid.set_active_target(target_setpoint)
     deadzone=pid.deadzone
     
     print(f"Starting simulation. Target: {target_setpoint}, Initial State: {current_state}\n")
+
+    loop_number = 1
     
-    for loop_number in range(1, 101):
+    while (True):
+
         error = target_setpoint - current_state
         
         pid_output = pid.compute(current_state)
         
-        current_state += pid_output * 0.1
+        current_state += pid_output * dt
         
-        print(f"***** Loop {loop_number:2d} *****")
+        print(f"***** Loop {loop_number :2d} *****")
         print(f"  Error:  {error:7.4f}")
         print(f"  Output: {pid_output:7.4f}")
         print(f"  State:  {current_state:7.4f}")
         print("-" * 30)
         time.sleep(0.02)
+
+        loop_number +=1
+
